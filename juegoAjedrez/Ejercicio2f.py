8ba8007c070dabab51a533dad0ab44e0eb4da6b0
diego pacori <dpacoria@unsa.edu.pe>
2022-06-02 08:29:04 -0500

    Se agrego el problema f

--- juegoAjedrez/Ejercicio2f.py
+++ juegoAjedrez/Ejercicio2f.py
@@ -1,2 +1,6 @@
 from interpreter import draw
 from chessPictures import *
+base1 = square.join(square.negative()).horizontalRepeat(3)
+base2 = square.negative().join(square).horizontalRepeat(3)
+baseT = Picture(base1.img + base2.img)
+draw(baseT.verticalRepeat(1))

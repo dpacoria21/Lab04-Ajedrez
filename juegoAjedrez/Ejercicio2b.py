3dd1aef0d0f666d43dc914a0eddc055037f41cb9
diego pacori <dpacoria@unsa.edu.pe>
2022-06-02 08:25:52 -0500

    Se agrego el problema 2

--- juegoAjedrez/Ejercicio2b.py
+++ juegoAjedrez/Ejercicio2b.py
@@ -1,2 +1,6 @@
 from interpreter import draw
 from chessPictures import *
+part1 = knight.join(knight.negative())
+part2 = knight.negative().verticalMirror().join(knight.verticalMirror())
+res = Picture(part1.img + part2.img)
+draw(res)
